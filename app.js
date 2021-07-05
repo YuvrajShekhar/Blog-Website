74480c2432a93c2c135aa89e175537e8c6e1f417
yuvraj <syuvraj1999@gamil.com>
2021-07-06 00:59:15 +0530

    added home,about, contact routes and enables to post title and body of a blog

--- app.js
+++ app.js
@@ -14,17 +14,38 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
 
+  res.render('home', {homeStartingContent : homeStartingContent});
+});
 
+app.get("/about", function (req, res) {
 
+  res.render('about', {aboutContent :aboutContent});
+});
 
+app.get("/contact", function (req, res) {
 
+  res.render('contact', {contactContent:contactContent});
+});
 
+app.get("/compose", function (req, res) {
 
+  res.render('compose');
+});
 
+app.post("/compose", function (req, res) {
 
+  const post ={
+    title : req.body.postTitle,
+    content: req.body.postBody
+  };
 
+  console.log(post.title);
+  console.log(post.content);
+  // res.redirect("/compose");
 
+})
 
 
 app.listen(3000, function() {
